7b3e4707bd8ef256683a7697e96d9d47291b0475
infosirdel1 <info.sirdel@gmail.com>
2025-12-03 22:28:41 +0100

    Update create-lead.js

--- api/create-lead.js
+++ api/create-lead.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+// ⭐ ID du produit test dans Odoo
+const PRODUCT_ID_TEST = 9;
+
 export default async function handler(req, res) {
   // --- CORS ---
   res.setHeader("Access-Control-Allow-Origin", "*");
